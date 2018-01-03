81a44215459dc53ec0927d546e24119bc6bd89b4
yyc <395976266@qq.com>
2018-01-03 10:57:05 +0800

    perf(renderTest gulp task): if current generated data's commit id == target's, not generate

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,47 @@ var fs = require("fs");
 
 var testRender = require("./lib/js/test/TestRender.js");
 
+function _runTest(done) {
+    console.log("run test");
+
+    testRender.runTest().then(function () {
+        console.log("done");
+        done()
+    }, function (e) {
+        console.log("fail");
+        console.error(e);
+        done();
+    })
+}
+
+function _deepCopyJson(json) {
+    return JSON.parse(JSON.stringify(json));
+}
+
+function _writeGenerateBasedCommitIdToConfig(commitId, config, configFilePath) {
+    var copiedConfig = _deepCopyJson(config);
+    copiedConfig.render.last_generate_based_commit_id = commitId;
+    fs.writeFileSync(configFilePath, copiedConfig);
+}
+
 gulp.task("testRender", function (done) {
+    var configFilePath = "./e2eConfig.json";
+
     git.revParse({ args: "HEAD" }, function (err, commitId) {
         var currentCommitId = commitId;
 
-        var basedCommitId = JSON.parse(fs.readFileSync("./e2eConfig.json")).render.commit_id;
+        var config = JSON.parse(fs.readFileSync(configFilePath));
+        var basedCommitId = config.render.base_commit_id;
 
         if (!!err) {
             console.error(err);
             done();
             return;
+        }
+
+        if (basedCommitId === config.render.last_generate_based_commit_id) {
+            _runTest(done);
+            return
         }
 
         console.log("reset hard to basedCommitId:", basedCommitId);
@@ -29,6 +60,8 @@ gulp.task("testRender", function (done) {
             console.log("reset hard to currentCommitId:", currentCommitId);
 
             testRender.generate().then(function () {
+                _writeGenerateBasedCommitIdToConfig(basedCommitId, config, configFilePath);
+
                 git.reset(currentCommitId, { args: '--hard' }, function (err) {
                     if (!!err) {
                         console.error(err);
@@ -36,16 +69,7 @@ gulp.task("testRender", function (done) {
                         return;
                     }
 
-                    console.log("run test");
-
-                    testRender.runTest().then(function () {
-                        console.log("done");
-                        done()
-                    }, function (e) {
-                        console.log("fail");
-                        console.error(e);
-                        done();
-                    })
+                    _runTest(done);
                 });
             }, function (e) {
                 console.error(e);
